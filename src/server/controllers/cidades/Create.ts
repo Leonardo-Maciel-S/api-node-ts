1fa1f2562e72c5b2d5f4d378316b293c02c3c333
Leonardo Maciel <leonardomacielj@gmail.com>
2025-03-21 14:47:39 -0300

    feat: install yup and started validations

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,27 @@
 import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 
+import * as yup from "yup";
+
 interface ICidade {
   name: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  const { name } = req.body;
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
 
-  res.status(StatusCodes.CREATED).json({ name });
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined = undefined;
+
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    res.status(StatusCodes.BAD_REQUEST).json({ errors: yupError.errors });
+    return;
+  }
+
+  res.status(StatusCodes.CREATED).json(validateData);
+  return;
 };
